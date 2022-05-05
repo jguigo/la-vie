f0c2aef730e9b6fd2513f09e47ed50993a49ddaf
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-05 18:56:24 -0300

    refactor: mudanças na relação

--- src/models/index.js
+++ src/models/index.js
@@ -2,14 +2,19 @@ const Pacientes = require("./Pacientes");
 const Psicologos = require("./Psicologos");
 const Atendimentos = require("./Atendimentos");
 
-Psicologos.belongsToMany(Pacientes, {
-   foreignKey: "psicologo_id",
-   through: Atendimentos
-})
-Pacientes.belongsToMany(Psicologos, {
-   foreignKey: "paciente_id",
-   through: Atendimentos
-})
+Psicologos.hasMany(Atendimentos, {
+   foreignKey: 'psicologo_id'
+ })
+Atendimentos.belongsTo(Psicologos, {
+   foreignKey: 'psicologo_id'
+ })
+
+Pacientes.hasMany(Atendimentos, {
+   foreignKey: 'paciente_id'
+ })
+Atendimentos.belongsTo(Pacientes, {
+   foreignKey: 'paciente_id'
+ })
 
 module.exports = {
    Psicologos,
